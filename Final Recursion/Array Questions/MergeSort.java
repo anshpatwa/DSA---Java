dc16582d461991eb99f054dab60557d2c266d0c2
Ansh <anshpatwa62@gmail.com>
2024-10-08 20:52:57 +0530

    erge sort updated

--- Final Recursion/Array Questions/MergeSort.java
+++ Final Recursion/Array Questions/MergeSort.java
@@ -35,10 +35,51 @@ public class MergeSort {
         }
         return mix;
     }
-    
+    static void mergesortInPlace(int[]arr, int s, int e){
+        if (e-s==1){return;}
+        int mid = (s+e)/2;
+        mergesort(Arrays.copyOfRange(arr,s,mid));
+        mergesort(Arrays.copyOfRange(arr,mid,e));
+        mergeInPlace(arr,s,mid,e);
+    }
+    static void mergeInPlace(int[] arr, int s, int m, int e){
+        int[] mix = new int[e-s];
+        int ptr1 =s;
+        int ptr2= m;
+        int ptr3 =0;
+        while (ptr1<m && ptr2 <e ){
+            if (arr[ptr1]<arr[ptr2]){
+                mix[ptr3] = arr[ptr1];
+                ptr1++;
+            }else{
+                mix[ptr3] =arr[ptr2];
+                ptr2++;
+            }
+            ptr3++;
+        }
+        while (ptr1<m){
+            mix[ptr3] = arr[ptr1];
+            ptr1++;
+            ptr3++;
+        }
+        while (ptr2<e){
+            mix[ptr3] = arr[ptr2];
+            ptr2++;
+            ptr3++;
+        }
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
         arr = mergesort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        // merge in place
+        mergesortInPlace(arr,0,arr.length);
+        System.out.println("Merge in Place");
         System.out.println(Arrays.toString(arr));
     }
 }
