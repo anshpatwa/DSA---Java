0545b86a5fbec4d466c0e06581b6f392dc002207
Ansh <anshpatwa62@gmail.com>
2024-10-19 20:39:22 +0530

    Permutations Updated

--- Final Recursion/Permutations/permutations_in_string.java
+++ Final Recursion/Permutations/permutations_in_string.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class permutations_in_string {
     static void permutations(String empty, String processed){
         if (processed.isEmpty()){
@@ -12,7 +14,39 @@ public class permutations_in_string {
         }
 
     }
+    static ArrayList<String> permuationslist(String empty,String processed){
+        if (processed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(empty);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        char ch = processed.charAt(0);
+        for (int i = 0; i <= empty.length(); i++) {
+            String first = empty.substring(0,i);
+            String second = empty.substring(i,empty.length());
+            ans.addAll(permuationslist(first+ch+second,processed.substring(1)));
+
+        }
+        return ans;
+    }
+    static int permuationsCount(String empty, String processed){
+        if (processed.isEmpty()){
+            return 1;
+        }
+        int count =0;
+        char ch = processed.charAt(0);
+        for (int i = 0; i <= empty.length(); i++) {
+            String first = empty.substring(0,i);
+            String second = empty.substring(i,empty.length());
+            count=count + permuationsCount(first+ch+second,processed.substring(1));
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        permutations("", "abc");
+       // permutations("", "abc");
+        ArrayList<String> permutations = permuationslist("","ABC");
+        System.out.println(permutations);
+        System.out.println(permuationsCount("","ABC"));
     }
 }
