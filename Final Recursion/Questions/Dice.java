257be9a57bf35b3dada821da338d19532e0593a6
Ansh <anshpatwa62@gmail.com>
2024-10-21 20:54:55 +0530

    diceFaces added

--- Final Recursion/Questions/Dice.java
+++ Final Recursion/Questions/Dice.java
@@ -23,7 +23,16 @@ public class Dice {
         }
         return list;
     }
+    static void diceFace(String p, int target, int face) {
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
 
+        for (int i = 1; i <= face && i <= target; i++) {
+            diceFace(p + i, target - i, face);
+        }
+    }
     public static void main(String[] args) {
         dice("", 4);
         System.out.println(diceRet("", 4));
