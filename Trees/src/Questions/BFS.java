2b16b194e60ce9eea69c1026e9f88539d86302ee
Ansh <anshpatwa62@gmail.com>
2024-12-13 15:32:51 +0530

    BFS Questions Added

--- Trees/src/Questions/BFS.java
+++ Trees/src/Questions/BFS.java
@@ -1,195 +1,316 @@
 package Questions;
+
 import java.util.*;
 // Breadth First Search -graph traversal algorithm -Visting nodes of Every Level
 
 
 public class BFS {
-    static class Node {
-        Node root;
-        int data;
-        Node left, right;
-        public Node(int item) {
-            data = item;
-            left = null;
-            right = null;
+    static private Node root;
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node next;
+        int height;
+
+        public Node(int value){
+            this.val = value;
+            this.next=null;
         }
     }
-    //Method 1 --> Naive Approach --> finding the height of the tree and traversing each level and printing the nodes of that level-->O(n^2)
-    // Method 2 --> Using Queue -->O(N)
-    public static  List<List<Integer>> bfstraversal(Node root){
-        List<List<Integer>> result = new ArrayList<>();
-        if(root ==null){
+        // BFS Representation
+    public List<List<Integer>> bfsrep(){
+        List<List<Integer>> results = new ArrayList<>();
+        if (root==null){
+            return results;
+        }
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+               Node curr =queue.poll();
+                level.add(curr.val);
+                if (curr.left!=null){
+                    queue.offer(curr.left);
+                } if (curr.right!=null){
+                    queue.offer(curr.right);
+                }
+            }
+            results.add(level);
+        }
+        return results;
+    }
+
+    // BFS in Reverse order
+    public List<List<Integer>> bfsrev(){
+        List<List<Integer>> results = new ArrayList<>();
+        if (root==null){
+            return results;
+        }
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                Node curr =queue.poll();
+                level.add(curr.val);
+                if (curr.left!=null){
+                    queue.offer(curr.left);
+                } if (curr.right!=null){
+                    queue.offer(curr.right);
+                }
+            }
+            results.add(0,level);
+        }
+        return results;
+    }
+
+    // Insert Level Wise in a tree(BFS Insert)
+    public void bfsInsert(int value){
+        Node newNode = new Node(value);
+        if (root==null){
+            root=newNode;
+            return;
+        }
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            Node curr = queue.poll();
+            if (curr.left==null){
+                curr.left = newNode;
+                return;
+            }else {
+                queue.offer(curr.left);
+            }
+            if (curr.right==null){
+                curr.right = newNode;
+                return;
+            }else {
+                queue.offer(curr.right);
+            }
+        }
+    }
+
+    // Average of Every Level of Tree
+    public List<List<Double>> avglvl(){
+
+        List<List<Double>> results = new ArrayList<>();
+        if (root == null){ return results;}
+
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int queuesize = queue.size();
+
+            List<Double> level = new ArrayList<>(queuesize);
+            double averagelvl = 0;
+
+            for (int i = 0; i < queuesize; i++) {
+                Node curr = queue.poll();
+                averagelvl += curr.val;
+                if (curr.left!=null){
+                    queue.offer(curr.left);
+                } if (curr.right!=null){
+                    queue.offer(curr.right);
+                }
+            }
+
+            averagelvl =averagelvl/(queuesize);
+            level.add(averagelvl);
+            results.add(level);
+
+        }
+        return results;
+    }
+
+
+    // Next Successor
+    public int nextsuccessor(int key){
+
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            int queuesize = queue.size();
+
+            for (int i = 0; i <queuesize ; i++) {
+                Node curr = queue.poll();
+                if (curr.val==key){
+                    return queue.peek().val;
+                }
+                if (curr.left!=null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right!=null) {
+                    queue.offer(curr.right);
+                }
+            }
+        }
+        return -1;
+    }
+    // Pointing Every Node to its right Node in the same level
+    public List<String> nextpointer() {
+        List<String> result = new ArrayList<>();
+        if (root == null) {
             return result;
         }
         Queue<Node> queue = new LinkedList();
         queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelsize = queue.size();
-            List<Integer> currentlevel = new ArrayList<>(levelsize);
-            for (int i = 0; i < levelsize; i++) {
+        while (!queue.isEmpty()) {
+            int queuesize = queue.size();
+
+            for (int i = 1; i <= queuesize; i++) {
                 Node curr = queue.poll();
-                currentlevel.add(curr.data);
-                if (curr.left!=null){
+                result.add(String.valueOf(curr.val));
+
+                if (curr.left != null) {
+                    curr.left.next=(curr.left.next==null)?curr.right:null;
                     queue.offer(curr.left);
-                }if (curr.right!=null){
+                }
+                if (curr.right != null) {
+                    curr.right.next =(curr.next!=null)?curr.next.left:null;
                     queue.offer(curr.right);
                 }
             }
-            result.add(currentlevel);
-        }
-            return result;
-    }
+            result.add("#");
 
-
-    // Average of Levels in Binary Tree
-    public static  List<Double> avgoflevels(Node root){
-        List<Double> result = new ArrayList<>();
-        if(root ==null){
-            return result;
-        }
-        Queue<Node> queue = new LinkedList();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelsize = queue.size();
-            double averagelevel = 0;
-            for (int i = 0; i < levelsize; i++) {
-                Node curr = queue.poll();
-                averagelevel +=curr.data;
-                if (curr.left!=null){
-                    queue.offer(curr.left);
-                }if (curr.right!=null){
-                    queue.offer(curr.right);
-                }
-            }
-            averagelevel = averagelevel/levelsize;
-            result.add(averagelevel);
         }
         return result;
     }
-
-    // Finding the Next Successor according to the levels
-    public static Node nextsuccessor(Node root,int key){
-        List<Double> result = new ArrayList<>();
-        if(root ==null){
+    // Method 2
+    public Node nextpointer2(){
+        if (root==null){
             return null;
         }
-        Queue<Node> queue = new LinkedList();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelsize = queue.size();
-
-                Node curr = queue.poll();
-                if (curr.left!=null){
-                    queue.offer(curr.left);
-                }if (curr.right!=null){
-                    queue.offer(curr.right);
+       Node leftmost = root;
+        while (leftmost.left!=null){
+            Node Current = leftmost;
+            while (Current!=null){
+                Current.left.next=Current.right;
+                if (Current.next!=null){
+                    Current.right.next=Current.next.left;
                 }
-                if (curr.data == key){
-                    break;
-                }
+                Current=Current.next;
+            }
+            leftmost=leftmost.left;
         }
-        return queue.peek();
+        return root;
     }
 
-    // Traversing the Tree in ZigZag Manner
-    public static List<List<Integer>> zigzag(Node root){
-        List<List<Integer>> result = new ArrayList<>();
-        if(root ==null){
-            return result;
+    //Zig-Zag
+    public List<List<Integer>> Zig_Zag() {
+        List<List<Integer>> results = new ArrayList<>();
+        if (root == null) {
+            return results;
         }
-        Deque<Node> deque = new LinkedList();
-        boolean reverse =false;
-        deque.add(root);
+        boolean reverse = false;
+        Deque<Node> deque = new LinkedList<>();
+        deque.offer(root);
 
-        while(!deque.isEmpty()){
-            int levelsize = deque.size();
-
-            List<Integer> currentlevel = new ArrayList<>(levelsize);
-
-            for (int i = 0; i < levelsize; i++) {
-               if (!reverse){
-                   Node curr = deque.pollFirst();
-                   currentlevel.add(curr.data);
-                   if (curr.left!=null){
-                       deque.addLast(curr.left);
-                   }if (curr.right!=null){
-                       deque.addLast(curr.right);
-                   }
-               }
-               else{
-                   Node curr = deque.pollLast();
-                   currentlevel.add(curr.data);
-                   if (curr.right!=null){
-                       deque.addFirst(curr.right);
-                   }if (curr.left!=null){
-                       deque.addFirst(curr.left);
-                   }
-               }
+        while (!deque.isEmpty()) {
+            int dequesize = deque.size();
+            List<Integer> level = new ArrayList<>(dequesize);
+            for (int i = 0; i < dequesize; i++) {
+                if (!reverse) {
+                    Node curr = deque.removeFirst();
+                    level.add(curr.val);
+                    if (curr.left != null) {
+                        deque.addLast(curr.left);
+                    }
+                    if (curr.right != null) {
+                        deque.addLast(curr.right);
+                    }
+                } else {
+                    Node curr = deque.removeLast();
+                    level.add(curr.val);
+                    if (curr.right != null) {
+                        deque.addFirst(curr.right);
+                    }
+                    if (curr.left != null) {
+                        deque.addFirst(curr.left);
+                    }
+                }
             }
-
             reverse = !reverse;
-            result.add(currentlevel);
+            results.add(level);
         }
-        return result;
+        return results;
     }
-    // Levels In Reverse Order
-    public static  List<List<Integer>> levelOrderBottom(Node root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root ==null){
-            return result;
-        }
-        Queue<Node> queue = new LinkedList();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelsize = queue.size();
-            List<Integer> currentlevel = new ArrayList<>(levelsize);
-            for (int i = 0; i < levelsize; i++) {
-                Node curr = queue.poll();
-                currentlevel.add(curr.data);
-                if (curr.left!=null){
-                    queue.offer(curr.left);
-                }if (curr.right!=null){
-                    queue.offer(curr.right);
-                }
-            }
-            result.add(0,currentlevel);
-        }
-        return result;
+
+    // Is Cousin
+    public boolean isCousin(Node node,int x,int y){
+        Node xx = getNode(root,x);
+        Node yy = getNode(root,y);
+        return ((level(root,xx,0)==level(root,yy,0)) && (!issibling(root,xx,yy)));
+
     }
+    public Node getNode(Node node,int x){
+        if (node ==null){return null;}
+        if(node.val ==x){
+            return node;
+        }
+        Node finding = getNode(node.left,x);
+        if (finding!=null){
+            return finding;
+        }
+        return getNode(node.right,x);
+    }
+    public int level(Node node,Node x,int lvl){
+        if (node==null){return 0;}
+        if (node==x){
+            return lvl;
+        }
+        int counter = level(node.left,x,lvl+1);
+        if (counter!=0){ return counter;}
+        return level(node.right,x,lvl+1);
+    }
+    public boolean issibling(Node node,Node x ,Node y){
+        if (node==null){return false;}
+        return ((node.left==x && node.right==y)||(node.left==y && node.right==x)||issibling(node.left,x,y)|| issibling(node.right,x,y));
+    }
+
+
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
+       BFS tree = new BFS();
+       tree.bfsInsert(1);
+       tree.bfsInsert(2);
+       tree.bfsInsert(3);
+       tree.bfsInsert(4);
+       tree.bfsInsert(5);
+       tree.bfsInsert(6);
+       tree.bfsInsert(7);
 
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
+       // Printing a BFS Tree Levels
+        System.out.println(tree.bfsrep());
 
-        root.right.left = new Node(6);
-        root.right.right = new Node(7);
+        // Reverse Order
+        System.out.println(tree.bfsrev());
 
-        // BFS Basics
-        List<List<Integer>> result = bfstraversal(root);
-        for (List<Integer> list : result) {
-            System.out.print(list + ",");
-        }
-        //System.out.println(Arrays.toString(new List[]{result}));
+        // Print average of Every Level
+        System.out.println(tree.avglvl());
 
-        //Average Of Every Level in a Single Array
-        List<Double> avg = avgoflevels(root);
-        System.out.println();
-        System.out.println(Arrays.toString(new List[]{avg}));
+        //Finding Next Successor of a Node
+        System.out.println(tree.nextsuccessor(3));
 
-        // Next Successor
-        Node exp = nextsuccessor(root,3);
-        System.out.println(exp.data);
+        //Next Pointer
+            //Method 1
+            //System.out.println(tree.nextpointer());
+            //Method 2
+            //Node ref = tree.nextpointer2();
 
-        // Zig-Zag
-        List<List<Integer>> zigzag = zigzag(root);
-        System.out.println(Arrays.toString(new List[]{zigzag}));
+        //Zig-Zag
+        System.out.println(tree.Zig_Zag());
 
-        // Levels in Reverse Order
-        List<List<Integer>> reverselevels = levelOrderBottom(root);
-        System.out.println(Arrays.toString(new List[]{reverselevels}));
+        //Is Cousin Not Siblings
+        System.out.println(tree.isCousin(root,3,4));
+
+
     }
 
 }
@@ -200,4 +321,6 @@ public class BFS {
 BFS -----> https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 AVGLVL -----> https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 Zig-Zag ----> https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/submissions/1475574638/
+Next Right Pointer ---->https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+IsCousin --->https://leetcode.com/problems/cousins-in-binary-tree/description/
 */
